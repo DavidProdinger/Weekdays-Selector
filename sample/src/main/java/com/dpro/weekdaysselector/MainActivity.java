8619c8b151aa486de7c9423cdf918ff24eb541e6
Falke Design <florian.bischof.1998@gmail.com>
2018-10-25 10:44:42 +0200

    Sample: Changed app_name variable Updated Gradle Created Icon Updated MainActivity with all methods to test
    Library:
    Added Border option
    Custom days can be setted
    Fullsize compatible with custom days
    Created getter and setter for all options
    Weekend can have a other color
    Recurrence spinner can get programmatically and added everywhere
    Optinon avaible that only one day can be selected

--- sample/src/main/java/com/dpro/weekdaysselector/MainActivity.java
+++ sample/src/main/java/com/dpro/weekdaysselector/MainActivity.java
@@ -1,8 +1,14 @@
 package com.dpro.weekdaysselector;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.LinearLayout;
+import android.widget.Switch;
+import android.widget.TextView;
 
 import com.dpro.widgets.OnWeekRecurrenceChangeListener;
 import com.dpro.widgets.OnWeekdaysChangeListener;
@@ -10,23 +16,43 @@ import com.dpro.widgets.WeekdaysPicker;
 
 import java.util.Arrays;
 import java.util.Calendar;
+import java.util.LinkedHashMap;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+import static java.util.Calendar.FRIDAY;
+import static java.util.Calendar.SATURDAY;
+import static java.util.Calendar.SUNDAY;
+import static java.util.Calendar.THURSDAY;
+import static java.util.Calendar.TUESDAY;
+
+public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
+
+    private final static String TAG = "MainAcitivty";
+    private final LinkedHashMap<Integer, Boolean> mp = new LinkedHashMap<>();
+    private WeekdaysPicker widget;
+    private List<Integer> selected_days;
+
+    private TextView tv_selected_days;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        List<Integer> days = Arrays.asList(Calendar.MONDAY, Calendar.WEDNESDAY, Calendar.FRIDAY, Calendar.SUNDAY);
+        selected_days = Arrays.asList(Calendar.SATURDAY, Calendar.WEDNESDAY, Calendar.THURSDAY, Calendar.SUNDAY);
 
-        WeekdaysPicker widget = findViewById(R.id.weekdays);
-        widget.setSelectedDays(days);
+        mp.put(SUNDAY, true);
+        mp.put(SATURDAY, true);
+        mp.put(THURSDAY, false);
+        mp.put(FRIDAY, true);
+        mp.put(TUESDAY, true);
+        mp.put(SATURDAY, false); //For duplicated values, the last one is counting
+
+        widget = findViewById(R.id.weekdays);
         widget.setOnWeekdaysChangeListener(new OnWeekdaysChangeListener() {
             @Override
             public void onChange(View view, int clickedDayOfWeek, List<Integer> selectedDays) {
-                // Do Something
+                tv_selected_days.setText("Selected days: " + Arrays.toString(selectedDays.toArray()));
             }
         });
         widget.setOnWeekRecurrenceChangeListener(new OnWeekRecurrenceChangeListener() {
@@ -45,7 +71,101 @@ public class MainActivity extends AppCompatActivity {
                         // if even weeks selected
                         break;
                 }
+                Log.d(TAG, "WeekRecurrence changed " + even_week + " selected days: " + Arrays.toString(selectedDays.toArray()));
             }
         });
+
+        LinearLayout weekrecurrenceview = findViewById(R.id.weekrecurrenceview);
+        weekrecurrenceview.addView(WeekdaysPicker.getRecurrenceSpinner(widget, this, weekrecurrenceview));
+
+        ((Switch) findViewById(R.id.sw_enable)).setOnCheckedChangeListener(this);
+        ((Switch) findViewById(R.id.sw_highlight_color)).setOnCheckedChangeListener(this);
+        ((Switch) findViewById(R.id.sw_background_color)).setOnCheckedChangeListener(this);
+        ((Switch) findViewById(R.id.sw_weekend_color)).setOnCheckedChangeListener(this);
+        ((Switch) findViewById(R.id.sw_text_color)).setOnCheckedChangeListener(this);
+        ((Switch) findViewById(R.id.sw_sunday_first)).setOnCheckedChangeListener(this);
+        ((Switch) findViewById(R.id.sw_show_weekend)).setOnCheckedChangeListener(this);
+        ((Switch) findViewById(R.id.sw_recurrence)).setOnCheckedChangeListener(this);
+        ((Switch) findViewById(R.id.sw_weekenddarker)).setOnCheckedChangeListener(this);
+        ((Switch) findViewById(R.id.sw_weekend_highlight)).setOnCheckedChangeListener(this);
+        ((Switch) findViewById(R.id.sw_border_color)).setOnCheckedChangeListener(this);
+        ((Switch) findViewById(R.id.sw_border_thickness)).setOnCheckedChangeListener(this);
+        ((Switch) findViewById(R.id.sw_border_highlight_color)).setOnCheckedChangeListener(this);
+        ((Switch) findViewById(R.id.sw_border_highlight_thickness)).setOnCheckedChangeListener(this);
+        ((Switch) findViewById(R.id.sw_fullsize)).setOnCheckedChangeListener(this);
+        ((Switch) findViewById(R.id.sw_customdays)).setOnCheckedChangeListener(this);
+        ((Switch) findViewById(R.id.sw_selectonlyone)).setOnCheckedChangeListener(this);
+        ((Switch) findViewById(R.id.sw_setselecteddays)).setOnCheckedChangeListener(this);
+
+
+        tv_selected_days = findViewById(R.id.tv_selected_days);
+        tv_selected_days.setText("Selected days: " + Arrays.toString(widget.getSelectedDays().toArray()));
+
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        int id = buttonView.getId();
+        switch (id) {
+            //selectDay
+            case R.id.sw_enable:
+                widget.setEditable(isChecked);
+                break;
+            case R.id.sw_highlight_color:
+                widget.setHighlightColor(isChecked ? Color.BLACK : Color.RED);
+                break;
+            case R.id.sw_background_color:
+                widget.setBackgroundColor(isChecked ? Color.YELLOW : Color.LTGRAY);
+                break;
+            case R.id.sw_weekend_color:
+                widget.setWeekendColor(isChecked ? Color.GREEN : Color.GRAY);
+                break;
+            case R.id.sw_text_color:
+                widget.setTextColor(isChecked ? Color.BLUE : Color.WHITE);
+                break;
+            case R.id.sw_sunday_first:
+                widget.setSundayFirstDay(isChecked);
+                break;
+            case R.id.sw_show_weekend:
+                widget.setShowWeekend(isChecked);
+                break;
+            case R.id.sw_recurrence:
+                widget.setRecurrence(isChecked);
+                break;
+            case R.id.sw_weekenddarker:
+                widget.setWeekendDarker(isChecked);
+                break;
+            case R.id.sw_weekend_highlight:
+                widget.setWeekendTextColor(isChecked ? Color.BLACK : Color.WHITE);
+                break;
+            case R.id.sw_border_color:
+                widget.setBorderColor(isChecked ? Color.BLUE : -1);
+                break;
+            case R.id.sw_border_thickness:
+                widget.setBorderThickness(isChecked ? 10 : 3);
+                break;
+            case R.id.sw_border_highlight_color:
+                widget.setBorderHighlightColor(isChecked ? Color.MAGENTA : -1);
+                break;
+            case R.id.sw_border_highlight_thickness:
+                widget.setBorderHighlightThickness(isChecked ? 10 : 3);
+                break;
+            case R.id.sw_fullsize:
+                widget.setFullSize(isChecked);
+                break;
+            case R.id.sw_customdays:
+                widget.setCustomDays(isChecked ? mp : null);
+                break;
+            case R.id.sw_selectonlyone:
+                widget.setSelectOnlyOne(isChecked);
+                break;
+            case R.id.sw_setselecteddays:
+                if (isChecked) {
+                    widget.setSelectedDays(selected_days);
+                    tv_selected_days.setText("Selected days: " + Arrays.toString(widget.getSelectedDays().toArray()));
+                }
+                break;
+        }
+        widget.redrawDays();
     }
 }
